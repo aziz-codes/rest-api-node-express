99a4ad5330c32df2e2f6f08b2da955a04fcffa8e
aziz-codes <azizisoffline@gmail.com>
2023-11-17 23:07:36 +0500

    deleted json file, sending user as an normal array

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,11 @@
-import user from "../user.json" assert { type: "json" };
+const users = [
+  { name: "user1", email: "user1@gmail.com" },
+  { name: "user2", email: "user2@gmail.com" },
+  { name: "user3", email: "user3@gmail.com" },
+  { name: "user4", email: "user4@gmail.com" },
+  { name: "user5", email: "user5@gmail.com" },
+];
 
 export const getUsers = (req, res) => {
-  res.send(user);
+  res.send(users);
 };
