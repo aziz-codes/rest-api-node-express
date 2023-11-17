59ee6075face9b01004f232b3eeb70fe803e7f1b
aziz-codes <azizisoffline@gmail.com>
2023-11-17 23:16:33 +0500

    defining a save user route

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { getUsers } from "../controllers/users.js";
+import { getUsers, saveUser } from "../controllers/users.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
-router.post("/");
+router.post("/save-user", saveUser);
 export default router;
